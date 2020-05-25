7a588782335026981f7077d16a71497142f0e3f3
Ariel Huynh <arielhuynh@ymail.com>
2020-05-25 14:56:46 -0700

    fixed PORT in server.js

--- client/server.js
+++ client/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+
+
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
@@ -13,4 +15,6 @@ app.get('/ping', function (req, res) {
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(port);
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
